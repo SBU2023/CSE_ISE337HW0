87e88e52d224bc380bbcf2595cbc5511622c5050
SBU2023 <140561879+SBU2023@users.noreply.github.com>
2023-08-25 08:14:34 -0400

    Update hw0.py

--- hw0.py
+++ hw0.py
@@ -2,5 +2,10 @@
 ### REPLACE THIS WITH SBU ID ###
 ### REPLACE THIS WITH NET ID ###
 
+### if you change the following functio
+### your test will fail. I would encourage
+### that you change it and observe the output
+### results.
+
 def msg():
-    return 'Hello World'
+    return 'Hello World' 
